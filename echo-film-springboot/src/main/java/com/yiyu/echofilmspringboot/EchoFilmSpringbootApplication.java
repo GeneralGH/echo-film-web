84a3499f3975ff802f8a0a693be1339f450f6874
xianer <740022475@qq.com>
2024-03-10 20:51:53 +0800

    后端初始化

---
+++ echo-film-springboot/src/main/java/com/yiyu/echofilmspringboot/EchoFilmSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yiyu.echofilmspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoFilmSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoFilmSpringbootApplication.class, args);
+    }
+
+}
---
+++ echo-film-springboot/src/test/java/com/yiyu/echofilmspringboot/EchoFilmSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yiyu.echofilmspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoFilmSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
