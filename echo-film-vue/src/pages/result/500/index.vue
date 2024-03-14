6d99bd3e400776261e74104dffd0e4fe51151862
740022475 <740022475@qq.com>
2024-03-14 14:01:34 +0800

    修改路由

--- echo-film-vue/src/pages/result/500/index.vue
+++ echo-film-vue/src/pages/result/500/index.vue
@@ -1,13 +1,5 @@
 <template>
-  <result title="500 Internal Server Error" type="500" :tip="$t('pages.result.500.subtitle')">
-    <t-button @click="() => $router.push('/')">{{ $t('pages.result.500.back') }}</t-button>
-  </result>
+  <div></div>
 </template>
-<script lang="ts">
-export default {
-  name: 'Result500',
-};
-</script>
-<script setup lang="ts">
-import Result from '@/components/result/index.vue';
-</script>
+
+<script lang="ts"></script>
