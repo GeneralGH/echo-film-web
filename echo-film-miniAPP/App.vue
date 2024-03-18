38b1c329f4c3b889ebb53fbaf021143aea4fbffa
740022475 <740022475@qq.com>
2024-03-18 16:58:43 +0800

    微信小程序端初始化

---
+++ echo-film-miniAPP/App.vue
@@ -0,0 +1,19 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+			console.log(uni.$u.config.v);
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "@/uni_modules/uview-ui/index.scss";
+	/*每个页面公共css */
+</style>
---
+++ echo-film-miniAPP/pages/tabbar/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<u-button type="primary" text="确定"></u-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
